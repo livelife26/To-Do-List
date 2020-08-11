0a5a517d48d323ee667d3bc12ceb2929e9862d02
lovelaught <darya.bratova13@gmail.com>
2020-08-11 17:26:50 +0300

    add to do list logic

--- src/doto-list/containers/DotoList/DotoList.jsx
+++ src/doto-list/containers/DotoList/DotoList.jsx
@@ -1,18 +1,61 @@
 import React, { useState } from "react";
 
 export const DotoList = () => {
-  const [counterValue, setCounterValue] = useState(0);
+  const [dotoList, setDotoList] = useState([]);
+
+  const addItem = () => {
+    const emptyItem = {
+      value: "",
+      isChecked: false,
+    };
+
+    setDotoList([...dotoList, emptyItem]);
+  };
 
   return (
     <div>
-      {counterValue}
+      {dotoList.map((listItem, index) => {
+        const { value, isChecked } = listItem;
+        const updatedList = dotoList.slice();
+
+        const onCheck = () => {
+          updatedList[index] = { ...listItem, isChecked: !isChecked };
+
+          setDotoList(updatedList);
+        };
+
+        const onEdit = (event) => {
+          updatedList[index] = { ...listItem, value: event.target.value };
+
+          setDotoList(updatedList);
+        };
+
+        const onRemove = () => {
+          updatedList.splice(index, 1);
+
+          setDotoList(updatedList);
+        };
+
+        return (
+          <div>
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={onCheck}
+            />
+            <input type="text" value={value} onChange={onEdit} />
+            <button onClick={onRemove}>remove</button>
+          </div>
+        );
+      })}
       <button
         onClick={() => {
-          setCounterValue(counterValue + 1);
+          addItem();
         }}
       >
-        count
+        add
       </button>
     </div>
   );
 };
+
