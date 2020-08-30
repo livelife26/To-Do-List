8df1bc3026d641dd1743dec8d28779aa9d442b5d
lovelaught <darya.bratova13@gmail.com>
2020-08-31 01:17:38 +0300

    change classes names

--- src/doto-list/containers/DotoList/DotoList.jsx
+++ src/doto-list/containers/DotoList/DotoList.jsx
@@ -1,3 +1,4 @@
+import "./style.scss";
 import React, { useState } from "react";
 
 export const DotoList = () => {
@@ -13,49 +14,68 @@ export const DotoList = () => {
   };
 
   return (
-    <div>
-      {dotoList.map((listItem, index) => {
-        const { value, isChecked } = listItem;
-        const updatedList = dotoList.slice();
+    <div className="app__layout">
+      <div className="todolist__layout">
+        <div className="todolist__title">
+          <h1 className="todolist__title-content">To do</h1>
+        </div>
+        <div className="todolist__list">
+          {dotoList.map((listItem, index) => {
+            const { value, isChecked } = listItem;
+            const updatedList = dotoList.slice();
 
-        const onCheck = () => {
-          updatedList[index] = { ...listItem, isChecked: !isChecked };
+            const onCheck = () => {
+              updatedList[index] = { ...listItem, isChecked: !isChecked };
 
-          setDotoList(updatedList);
-        };
+              setDotoList(updatedList);
+            };
 
-        const onEdit = (event) => {
-          updatedList[index] = { ...listItem, value: event.target.value };
+            const onEdit = (event) => {
+              updatedList[index] = { ...listItem, value: event.target.value };
 
-          setDotoList(updatedList);
-        };
+              setDotoList(updatedList);
+            };
 
-        const onRemove = () => {
-          updatedList.splice(index, 1);
+            const onRemove = () => {
+              updatedList.splice(index, 1);
 
-          setDotoList(updatedList);
-        };
+              setDotoList(updatedList);
+            };
 
-        return (
-          <div>
-            <input
-              type="checkbox"
-              checked={isChecked}
-              onChange={onCheck}
-            />
-            <input type="text" value={value} onChange={onEdit} />
-            <button onClick={onRemove}>remove</button>
-          </div>
-        );
-      })}
-      <button
-        onClick={() => {
-          addItem();
-        }}
-      >
-        add
-      </button>
+            return (
+              <div className="todolist__item todolist-item">
+                <label className="todolist-item__label">
+                  <input
+                    className="todolist-item__checkbox"
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={onCheck}
+                  />
+                  <span className="todolist-item__checkmark"></span>
+                </label>
+                <input
+                  placeholder="Type your new task!"
+                  className="todolist-item__form"
+                  type="text"
+                  value={value}
+                  onChange={onEdit}
+                />
+                <button className="todolist-item__remove" onClick={onRemove}></button>
+              </div>
+            );
+          })}
+        </div>
+        <div className="todolist__add">
+          <button
+            className="todolist__add-button"
+            onClick={() => {
+              addItem();
+            }}
+          >
+            New task
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
-
