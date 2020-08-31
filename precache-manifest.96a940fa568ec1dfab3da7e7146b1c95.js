27c0b7f9ef1f2101a75bd2a7e9361f5fc7861c08
lovelaught <darya.bratova13@gmail.com>
2020-08-31 23:01:33 +0300

    Updates

---
+++ precache-manifest.96a940fa568ec1dfab3da7e7146b1c95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe49885fd59cb9f48447f955b50f542",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "52bd97dfb84be709f068",
+    "url": "/To-Do-List/static/css/main.934fd731.chunk.css"
+  },
+  {
+    "revision": "4463bddd28bc6437fdd9",
+    "url": "/To-Do-List/static/js/2.d114e9d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.d114e9d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52bd97dfb84be709f068",
+    "url": "/To-Do-List/static/js/main.9346b304.chunk.js"
+  },
+  {
+    "revision": "9b2856a6783a38429be5",
+    "url": "/To-Do-List/static/js/runtime-main.791e09c1.js"
+  }
+]);
